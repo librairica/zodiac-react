4fd1d2de20decd100fcf2e3ba6a7c232bc65a2a2
librairica <erica.handelman@gmail.com>
2022-08-23 20:40:37 -0400

    style form and conditionally render contacts vs form

--- src/AddContactForm.js
+++ src/AddContactForm.js
@@ -1,64 +1,92 @@
-import "./AddContactForm.css"
+import "./AddContactForm.css";
 import React from "react";
-import { useMutation, gql } from '@apollo/client';
-import { useState } from 'react';
+import { useMutation, gql } from "@apollo/client";
+import { useState } from "react";
 
 const CREATE_CONTACT = gql`
-	mutation Mutation($contactInput: ContactInput) {
-		createContact(ContactInput: $contactInput) {
-			id
-			firstName
-			lastName
-			birthday
-			# birthTimestamp
-			# sunSign
-			# sunSignEmoji
-		}
-	}
+  mutation Mutation($contactInput: ContactInput) {
+    createContact(ContactInput: $contactInput) {
+      id
+      firstName
+      lastName
+      birthday
+      # birthTimestamp
+      # sunSign
+      # sunSignEmoji
+    }
+  }
 `;
 
-
 export function AddContactForm() {
-	const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({});
 
-	const handleChange = (event) => {
+  const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
   const [createContact, { data, loading, error }] = useMutation(CREATE_CONTACT);
 
-  if (loading) return 'Submitting...';
+  if (loading) return "Submitting...";
   if (error) return `Submission error! ${error.message}`;
 
   return (
-    <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-					alert(inputs);
-          createContact({ variables: { 
-						contactInput: { 
-							firstName: inputs.firstName,
-							lastName: inputs.lastName,
-							birthday: inputs.birthday
-						}} 
-					})
-					inputs = null;
-        }}
-      >
-        <label> First Name: 
-					<input type="text" name="firstName" value={inputs.firstName || ""} onChange={handleChange}/>
-				</label>
-				<label> Last Name: 
-					<input type="text" name="lastName" value={inputs.lastName || ""} onChange={handleChange}/>
-				</label>
-				<label> Birthday: 
-					<input type="date" name="birthday" value={inputs.birthday || ""} onChange={handleChange}/>
-				</label>
-        <button type="submit">Add Contact</button>
-      </form>
+    <div id="form-container">
+      <div id="add-contact-form">
+				<h2>New Contact</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            alert(inputs);
+            createContact({
+              variables: {
+                contactInput: {
+                  firstName: inputs.firstName,
+                  lastName: inputs.lastName,
+                  birthday: inputs.birthday,
+                },
+              },
+            });
+            inputs = null;
+          }}
+        >
+          <div>
+						<label>
+							First Name:
+							<input
+								type="text"
+								name="firstName"
+								value={inputs.firstName || ""}
+								onChange={handleChange}
+							/>
+						</label>
+					</div>
+          <div>
+						<label>
+							Last Name:
+							<input
+								type="text"
+								name="lastName"
+								value={inputs.lastName || ""}
+								onChange={handleChange}
+							/>
+						</label>
+					</div>
+          <div>
+						<label>
+							Birthday:
+							<input
+								type="date"
+								name="birthday"
+								value={inputs.birthday || ""}
+								onChange={handleChange}
+							/>
+						</label>
+					</div>
+          <div class="button-container"><button type="submit">Add Contact</button></div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 // Import everything needed to use the `useQuery` hook
+import { useState } from 'react';
 import { AddContactForm } from './AddContactForm';
 import { DisplayContacts } from './ContactList';
-// import { useState } from 'react';
 
 export default function App() {
-  return (
-    <div class="container">
-      <div class="contacts-container">
-        <h2>Zodiac Contacts ✨</h2>
-        <br/>
-        <DisplayContacts />
-        <br/>
-        <button onClick={addContact}>Add a contact</button>
-        <br/>
-        <AddContactForm/>
+  const [showForm, setShowForm] = useState(false)
+  if(!showForm) {
+    return (
+      <div class="container">
+        <div class="contacts-container">
+          <h2>Zodiac Contacts ✨</h2>
+          <DisplayContacts />
+          <button onClick={() => setShowForm(!showForm)}>Add a contact</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <AddContactForm/>
+    )
+  }
 }
 
 function addContact() {
   console.log("open add contact form")
+  
 }
